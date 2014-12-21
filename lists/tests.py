4f47035b4f92168880697722f22845e3d4ae6f7f
Ahmad Rudi <ahm.rudi@gmail.com>
2014-12-21 17:58:26 +0700

    Tampilan dasar dengan html sederhana

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 # file lists/test.py
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -12,3 +14,15 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		# apakah ada function home_page disitu?
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		# memintta object halaman
+		request = HttpRequest()
+		# yang ada di home page
+		response = home_page(request)
+		# dimana halaman tersebut haruslah dimulai dengan html
+		self.assertTrue(response.content.startswith(b'<html>'))
+		# dengan judul To-Do lists
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		# dan diakhiri dengan kode html pula
+		self.assertTrue(response.content.endswith(b'</html>'))
